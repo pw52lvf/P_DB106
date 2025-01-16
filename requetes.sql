d423135335f6627a8ed095ecc56c6733ac6b0e59
pw52lvf <pw52lvf@eduvaud.ch>
2025-01-16 23:05:35 +0100

    requetes completes

--- requetes.sql
+++ requetes.sql
@@ -5,8 +5,8 @@ USE db_space_invaders;
 -- ADMIN
  
 CREATE ROLE 'Admin';
-GRANT ALL PRIVILEGES
-ON *.*
+GRANT CREATE, SELECT, UPDATE, DELETE
+ON db_space_invaders.*
 TO 'Admin'
 WITH GRANT OPTION;
  
@@ -23,36 +23,36 @@ TO 'Joueur';
  
 -- GESTIONNAIRE DE LA BOUTIQUE
  
-CREATE ROLE 'Gestionnaire de la boutique';
+CREATE ROLE 'gestionnaire_de_la_boutique';
 GRANT SELECT
 ON t_joueur
-TO 'Gestionnaire de la boutique';
+TO 'gestionnaire_de_la_boutique';
  
  
-GRANT ALL PRIVILEGES
+GRANT UPDATE, SELECT, DELETE
 ON t_arme
-TO 'Gestionnaire de la boutique';
+TO 'gestionnaire_de_la_boutique';
  
 GRANT SELECT
 ON t_commande
-TO 'Gestionnaire de la boutique';
+TO 'gestionnaire_de_la_boutique';
  
 -- Création des utilisateurs et attribution des rôles
  
 -- 1. Admin du jeu
-CREATE USER 'admin'@'%' IDENTIFIED BY 'password';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'smjGd2z8ah7hf5aw';
 GRANT 'Admin' TO 'admin'@'%';
 SET DEFAULT ROLE 'Admin' TO 'admin'@'%';
  
 -- 2. Joueur
-CREATE USER 'joueur'@'%' IDENTIFIED BY 'password';
+CREATE USER 'joueur'@'%' IDENTIFIED BY 'jGd2z8ahadsafg7ff5aw';
 GRANT 'Joueur' TO 'joueur'@'%';
 SET DEFAULT ROLE 'Joueur' TO 'joueur'@'%';
  
 -- 3. Gestionnaire de la boutique
-CREATE USER 'boutique'@'%' IDENTIFIED BY 'password';
-GRANT 'Gestionnaire de la boutique' TO 'boutique'@'%';
-SET DEFAULT ROLE 'Gestionnaire de la boutique' TO 'boutique'@'%';
+CREATE USER 'gestionnaire_boutique'@'%' IDENTIFIED BY 'g7ds9fsz8ah7hf5aw8dfsf';
+GRANT 'gestionnaire_de_la_boutique' TO 'gestionnaire_boutique'@'%';
+SET DEFAULT ROLE 'gestionnaire_de_la_boutique' TO 'boutique'@'%';
 
 -- Requête n°1 :
 SELECT * FROM `t_joueur` ORDER BY `jouNombrePoints` DESC LIMIT 5;
@@ -61,7 +61,7 @@ SELECT * FROM `t_joueur` ORDER BY `jouNombrePoints` DESC LIMIT 5;
 SELECT MAX(`armPrix`) AS PrixMaximum, AVG(`armPrix`) AS prixMoyen, MIN(`armPrix`) AS PrixMinimum FROM `t_arme`;
 
 -- Requête n°3 :
-SELECT DISTINCT `fkJoueur` AS "idJoueur", jouPseudo, COUNT(idCommande) AS "NombreCommandes" 
+SELECT DISTINCT `fkJoueur` AS "idJoueur", COUNT(idCommande) AS "NombreCommandes" 
 FROM t_commande 
 JOIN t_joueur ON t_joueur.idJoueur = t_commande.fkJoueur 
 GROUP BY fkJoueur 
@@ -74,13 +74,17 @@ GROUP BY fkJoueur
 HAVING COUNT(idCommande) > 2;
 
 -- Requête n°5 :
-SELECT DISTINCT jouPseudo, armNom 
+ SELECT DISTINCT jouPseudo, armNom /*, comNumeroCommande*/
 FROM t_detail_commande 
-JOIN t_joueur ON t_detail_commande.fkCommande = t_joueur.idJoueur 
-JOIN t_arme ON t_detail_commande.fkArme = t_arme.idArme;
+    JOIN t_joueur 
+        ON t_detail_commande.fkCommande = t_joueur.idJoueur 
+    JOIN t_arme 
+        ON t_detail_commande.fkArme = t_arme.idArme
+     JOIN t_commande                                    /*jointure manquante*/
+        ON t_commande.fkJoueur = t_joueur.idJoueur ;    
 
 -- Requête n°6 :
-SELECT idJoueur, SUM(armPrix) AS "TotalDepense" 
+SELECT idJoueur, SUM(detQuantiteCommande * armPrix) AS "TotalDepense" 
 FROM t_detail_commande 
 JOIN t_commande ON t_detail_commande.fkCommande = t_commande.idCommande 
 JOIN t_arme ON t_detail_commande.fkArme = t_arme.idArme 
@@ -91,7 +95,7 @@ ORDER BY TotalDepense DESC LIMIT 10;
 -- Requête n°7 :
 SELECT jouPseudo, idCommande 
 FROM `t_commande` 
-JOIN t_joueur ON t_commande.fkJoueur = t_joueur.idJoueur;
+RIGHT JOIN t_joueur ON t_commande.fkJoueur = t_joueur.idJoueur;
 
 -- Requête n°8 :
 SELECT idCommande, jouPseudo 
@@ -108,9 +112,9 @@ GROUP BY jouPseudo
 ORDER BY TotalQuantite DESC;
 
 -- Requête n°10 :
-SELECT jouPseudo, COUNT(idCommande) AS NombreCommandes 
+SELECT idJoueur, jouPseudo, COUNT(DISTINCT fkArme) AS NombreCommandes 
 FROM t_joueur 
 JOIN t_commande ON idJoueur = t_commande.fkJoueur 
 JOIN t_detail_commande ON t_commande.idCommande = t_detail_commande.fkCommande 
 GROUP BY jouPseudo 
-HAVING COUNT(DISTINCT t_detail_commande.fkArme) > 3;
+HAVING COUNT(DISTINCT fkArme) > 3;
